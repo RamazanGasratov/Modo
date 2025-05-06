3e0b769baa9ee4c28e1426e5e0a89a53b6bd5b8e
Ramazan g <gasratov99@bk.ru>
2025-05-06 17:05:23 +0300

    Initial commit from Xcode

---
+++ Modo/ModoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModoApp.swift
+//  Modo
+//
+//  Created by Рамазан Гасратов on 06.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct ModoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
